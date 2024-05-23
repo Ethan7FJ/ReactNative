028b8d7a29e3042acb52902bbf91d4bd12feff26
natalia <johanfetecua11@gmail.com>
2024-05-23 13:19:07 -0500

    ReactNative23/05/2024

--- App.tsx
+++ App.tsx
@@ -1,38 +1,105 @@
 import React from "react";
-import {View,Text,StyleSheet, TextInput} from "react-native";
+import { View,Text,TextInput,TouchableOpacity, StyleSheet,Dimensions, FlatList} from "react-native";
 
-const stilo = StyleSheet.create({
-
+const dibujar=StyleSheet.create({
   contenedor:{
-    width:'80%',
-    height: '50%',
+    width:'100%',
     padding:20,
-    marginLeft:'10%',
-    marginRight:'10%',
-    marginTop:'10%',
-    backgroundColor:"green",
-    justifyContent:"flex-end",
-    alignItems:"flex-end",
-    marginHorizontal: '10%'
   },
-  titulos:{
-    fontSize:20,
-    color:"White",
-  },
-  teinp:{
+  titulo:{
     fontSize:30,
+    color:'white',
+
+  },
+  txtimp:{
+    borderColor:'white',
+    borderWidth:1,
+    borderRadius:15,
+    width:Dimensions.get("screen").width*0.6,
+    paddingLeft:"0.025%",
+  },
+  text:{
+    fontSize:17,
+    color:'white',
+  },
+  wtext:{
+    fontSize:17,
+    color:'black',
+  },
+  impcontenedor:{
+    marginTop:20,
+    flexDirection:'row',
+    justifyContent:"space-between",
+  },
+  boton:{
+    width:Dimensions.get("screen").width*0.25,
+    backgroundColor:"#F8EBAE",
+    justifyContent:"center",
+    alignItems: "center",
+    borderRadius:15,
+  },
+  tareasconten:{
+    paddingVertical:20,
+    borderBottomColor: "white",
+    borderBottomWidth:1,    
+  },
+  botontitu:{
+    width:100,
   }
 })
 
-export default function App(){
+const Taareas = [
+  {
+    titulo:"Lavar Baño",
+    estado: false,
+    fecha: new Date(),
+  },
+  {
+    titulo:"Lavar Loza",
+    estado: false,
+    fecha: new Date(),
+  },
+  {
+    titulo:"Lavar Ropa",
+    estado: false,
+    fecha: new Date(),
+  },
+]
 
+interface Task{
+  titulo:String,
+  estado:boolean,
+  fecha:Date
+}
+
+function renderItem({item}:{item:Task}){
   return(
-      <View style={stilo.contenedor}>
-            <Text style={stilo.titulos}>Wuenas sunguita</Text>
-            <View>
-                <TextInput style={stilo.teinp} placeholder="Agregar"/>
-            </View>
+    <View style={dibujar.tareasconten}>
+      <TouchableOpacity style={dibujar.botontitu}>
+        <Text style={dibujar.text}>{item.titulo}</Text>
+      </TouchableOpacity>
+      <Text style={dibujar.text}>{item.fecha.toDateString()}</Text>
+    </View>  
+    )
+}
+
+export default function App(){
+  return(
+    <View style={dibujar.contenedor}>
+      <Text style={dibujar.titulo}>
+        Hola Johitan
+      </Text>
+      <View style={dibujar.impcontenedor}>
+        <TextInput style={dibujar.txtimp} placeholder="Agregar"/>
+        <TouchableOpacity style={dibujar.boton}>
+          <Text style={dibujar.wtext}>
+            Enviar
+          </Text>
+        </TouchableOpacity>
       </View>
-      
+      <View>
+        <FlatList data={Taareas} renderItem={renderItem}/>
+      </View>
+    </View>
   )
 }
